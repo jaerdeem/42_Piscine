10a052aa05efa08383c2844757cd1c76791b7087
Erdem "PortBranch" Alkan <80523209+portbranch@users.noreply.github.com>
2021-12-08 20:28:49 +0300

    C Piscine C_03 - OK 100% (08-12-2021)

---
+++ C_03/ex01/ft_strncmp.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aalkan <aalkan@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/08 17:18:56 by aalkan            #+#    #+#             */
+/*   Updated: 2021/12/08 17:20:00 by aalkan           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_strncmp(char *s1, char *s2, unsigned int n)
+{
+	unsigned int	index;
+
+	if (n == 0)
+		return (0);
+	index = 0;
+	while (s1[index] && s2[index] && s1[index] == s2[index] && index < n - 1)
+		index++;
+	return (s1[index] - s2[index]);
+}
