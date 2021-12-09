3a97b19560293305f1f412a008190bdefce5f613
Erdem "PortBranch" Alkan <80523209+portbranch@users.noreply.github.com>
2021-12-09 13:14:25 +0300

    C Piscine C_05 - OK 90% (09-12-2021)

---
+++ C_05/ex00/ft_iterative_factorial.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aalkan <aalkan@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/09 13:07:58 by aalkan            #+#    #+#             */
+/*   Updated: 2021/12/09 13:08:01 by aalkan           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	ft_iterative_factorial(int nb)
+{
+	int	res;
+
+	res = 1;
+	if (nb < 0)
+		return (0);
+	else if (nb <= 1)
+		return (1);
+	while (nb > 0)
+		res *= nb--;
+	return (res);
+}
---
+++ C_05/ex07/ft_find_next_prime.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_find_next_prime.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aalkan <aalkan@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/09 13:10:36 by aalkan            #+#    #+#             */
+/*   Updated: 2021/12/09 13:10:39 by aalkan           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_is_prime(int nb)
+{
+	int	i;
+
+	i = 2;
+	if (nb < 2)
+		return (0);
+	while (i <= nb / i)
+	{
+		if (nb % i == 0)
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int	ft_find_next_prime(int nb)
+{
+	if (nb < 2)
+		return (2);
+	while (nb >= 2)
+	{
+		if (ft_is_prime(nb) == 1)
+			return (nb);
+		nb++;
+	}
+	return (0);
+}
