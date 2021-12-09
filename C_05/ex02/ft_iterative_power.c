0598aca9a8aeda802e311d24c77462e9166a6836
Port <80523209+portbranch@users.noreply.github.com>
2021-12-09 20:10:05 +0300

    C Piscine C_05 - OK 80% (09-12-2021)

---
+++ C_05/ex02/ft_iterative_power.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_power.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aalkan <aalkan@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/09 13:08:43 by aalkan            #+#    #+#             */
+/*   Updated: 2021/12/09 13:08:45 by aalkan           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_iterative_power(int nb, int power)
+{
+	int	result;
+
+	result = nb;
+	while (power > 1)
+	{
+		result *= nb;
+		power--;
+	}
+	if (power == 0)
+		return (1);
+	if (power < 0)
+		return (0);
+	return (result);
+}
