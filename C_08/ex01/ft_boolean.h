f39e1f9f6c7b4b1498e069b6dc4c02785a766c97
Port <80523209+portbranch@users.noreply.github.com>
2021-12-15 12:25:38 +0300

    C Piscine C_08 - OK 100% (14-12-2021)

---
+++ C_08/ex01/ft_boolean.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_boolean.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aalkan <aalkan@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/14 16:37:31 by aalkan            #+#    #+#             */
+/*   Updated: 2021/12/14 16:37:35 by aalkan           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_BOOLEAN_H
+# define FT_BOOLEAN_H
+
+# include <unistd.h>
+
+typedef char	t_bool;
+# define FALSE 0
+# define TRUE 1
+
+# define SUCCESS 0
+# define EVEN_MSG "I have an even number of arguments.\n"
+# define ODD_MSG "I have an odd number of arguments.\n"
+
+# define EVEN(X) ((X % 2) ? 0 : 1)
+
+#endif
